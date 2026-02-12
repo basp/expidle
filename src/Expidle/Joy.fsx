c6ab7fd701c4f04113848a0e976dd433ce6b19bf
Bas Pennings <baspster@gmail.com>
2026-02-12 19:51:31 +0100

    Basic data model. Initial Toy Joy commit.

--- src/Expidle/Joy.fsx
+++ src/Expidle/Joy.fsx
@@ -1,1 +1,22 @@
-﻿
+﻿type JoyValue =
+    | Bool of bool
+    | Int of int
+    | String of string
+    | Symbol of string
+    | Quotation of JoyValue list
+       
+type Runtime = {
+    Queue: JoyValue list
+    Stack: JoyValue list
+    Env: Map<string, JoyDefinition>
+    // TODO:
+    // Trace: TraceEntry list
+}
+
+and JoyDefinition =
+    | Builtin of Builtin
+    | Defined of JoyValue list
+    
+and Builtin = Runtime -> Runtime
+
+    
